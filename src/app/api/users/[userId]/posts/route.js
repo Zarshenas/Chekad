dc03fbdaab006848e4502e5e50412461e5bf4400
Ali Zarshenas <ali.zarshenas.2018@gmail.com>
2024-12-28 20:36:14 +0330

    user posts API refactored
    error handling improved also responses translated to Persian

--- src/app/api/users/[userId]/posts/route.js
+++ src/app/api/users/[userId]/posts/route.js
@@ -1,77 +1,81 @@
 import { newPostSchema } from "@/helpers/validation/newPostSchema";
 import { Post } from "@/models/Post";
-import { cloudinary, moveFilesToPostFolder } from "@/utils/cloudinary";
+import { cloudinary } from "@/utils/cloudinary";
 import connectDB from "@/utils/connectDB";
 import { withMulter } from "@/utils/multer";
 import mongoose from "mongoose";
 import { NextResponse } from "next/server";
 import { Readable } from "stream";
 
-export const GET =async (req, {params}) => {
-  const {userId} = params;
+export const GET = async (req, { params }) => {
+  const { userId } = params;
+
   try {
-    connectDB();
-    const fetchAllPosts = await Post.find({userId :new mongoose.Types.ObjectId(userId)})
-      .then((data) => data)
-      .catch((error) => {
-        throw new Error("couldn't retrieve posts");
-      });
-      return NextResponse.json({ post:fetchAllPosts }, { status: 200 });
+    await connectDB();
+
+    if (!mongoose.Types.ObjectId.isValid(userId)) {
+      return NextResponse.json(
+        { error: "آیدی وارد شده معتبر نیست." },
+        { status: 400 }
+      );
+    }
+
+    const userPosts = await Post.find({ userId: new mongoose.Types.ObjectId(userId) });
+
+    return NextResponse.json({ posts: userPosts }, { status: 200 });
   } catch (error) {
-    console.log(error);
-    return NextResponse.json({ error: error }, { status: 500 });
+    console.error("Error fetching posts:", error);
+    return NextResponse.json(
+      { error: "خطایی در بازیابی پست‌ها رخ داد." },
+      { status: 500 }
+    );
   }
 };
 
-export const POST = withMulter(async (req, res) => {
+export const POST = withMulter(async (req) => {
   const userId = req.headers.get("x-user-id");
   const postsFolder = `chekad-project/users/${userId}/posts`;
-  if (!mongoose.Types.ObjectId.isValid(userId)) {
-    return new Response(JSON.stringify({ error: "Not Authenticated" }), {
-      status: 401,
-    });
-  }
+
   try {
+    if (!mongoose.Types.ObjectId.isValid(userId)) {
+      return NextResponse.json(
+        { error: "شما احراز هویت نشده‌اید." },
+        { status: 401 }
+      );
+    }
+
     const formData = await req.formData();
     const title = formData.get("title");
     const content = formData.get("content");
     const tags = formData.getAll("tags");
     const files = formData.getAll("files");
     const image = formData.get("image");
-    console.log(files);
-    //   Connect to the database
+
     await connectDB();
-    
-    const validatedPost = await newPostSchema
-    .validate({
+
+    const validatedPost = await newPostSchema.validate({
       userId,
       title,
       content,
       tags,
       files,
       image,
-    })
-    .then((data) => data)
-    .catch((error) => {
-      return error;
     });
-    console.log(validatedPost);
-    const fileUrl = await Promise.all(
+
+    const fileUrls = await Promise.all(
       validatedPost.files.map(async (file) => {
-        // Create a Readable stream from the file
         const readableStream = Readable.from(file.stream());
-        // Upload the file to Cloudinary
         const uploadResult = await new Promise((resolve, reject) => {
           const uploadStream = cloudinary.uploader.upload_stream(
-            // { folder: `user_${userId}_POSTS_FILES`, public_id: `${file.name}` },
             { folder: postsFolder, public_id: `${file.name.split(".")[0]}` },
             (error, result) => {
-              if (error) return reject(error);
+              if (error) reject(error);
               resolve(result);
             }
           );
-          readableStream.pipe(uploadStream); // Pipe the stream to Cloudinary
+          readableStream.pipe(uploadStream);
         });
+
         return {
           filename: file.name,
           size: file.size,
@@ -80,35 +84,35 @@ export const POST = withMulter(async (req, res) => {
         };
       })
     );
-    
-    // Create a Readable stream from the file
-    const readableStream = Readable.from(image.stream());
-    // Upload the file to Cloudinary
-    const uploadResult = await new Promise((resolve, reject) => {
+
+    const imageStream = Readable.from(image.stream());
+    const imageUploadResult = await new Promise((resolve, reject) => {
       const uploadStream = cloudinary.uploader.upload_stream(
-        { folder: `${postsFolder}`, public_id: `${image.name.split(".")[0]}` },
+        { folder: postsFolder, public_id: `${image.name.split(".")[0]}` },
         (error, result) => {
-          if (error) return reject(error);
+          if (error) reject(error);
           resolve(result);
         }
       );
-      readableStream.pipe(uploadStream); // Pipe the stream to Cloudinary
+      imageStream.pipe(uploadStream);
     });
-    
-    // console.log(uploadResult.secure_url);
+
     const newPost = await Post.create({
       ...validatedPost,
       userId: new mongoose.Types.ObjectId(userId),
-      imageUrl: uploadResult.secure_url,
-      files: fileUrl,
-    }).then((newPost) => newPost);
-    if (newPost) {
-      return new Response(JSON.stringify({ message: "Post created" }), {
-        status: 201,
-      });
-    }
+      imageUrl: imageUploadResult.secure_url,
+      files: fileUrls,
+    });
+
+    return NextResponse.json(
+      { message: "پست با موفقیت ایجاد شد.", post: newPost },
+      { status: 201 }
+    );
   } catch (error) {
-    console.log(error);
-    return NextResponse.json({ errors: error }, { status: 400 });
+    console.error("Error creating post:", error);
+    return NextResponse.json(
+      { error: "خطایی در ایجاد پست رخ داد. لطفاً دوباره تلاش کنید." },
+      { status: 400 }
+    );
   }
 });
