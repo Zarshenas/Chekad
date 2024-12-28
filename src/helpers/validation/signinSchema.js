0607d1973e7225f0c9503d4cb1b728b79c20113a
Ali Zarshenas <ali.zarshenas.2018@gmail.com>
2024-12-28 20:39:29 +0330

    schema error messages translated to Persian.

--- src/helpers/validation/signinSchema.js
+++ src/helpers/validation/signinSchema.js
@@ -2,10 +2,11 @@ import * as Yup from "yup";
 
 const signinSchema = Yup.object().shape({
   email: Yup.string()
-    .email("Invalid email format")
-    .required("Email is required"),
+    .email("فرمت ایمیل معتبر نیست")
+    .required("ایمیل الزامی است"),
   password: Yup.string()
-    .required("Password is required")
+    .required("رمز عبور الزامی است")
 });
+
 
 export default signinSchema;
