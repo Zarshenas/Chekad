050ec359348352bfa7f780a8ed99425816a17353
Ali Zarshenas <ali.zarshenas.2018@gmail.com>
2025-02-01 16:00:53 +0330

    gh-pages removed

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    unoptimized: true, // Disable default image optimization
-  },
-  assetPrefix: isProd ? "/your-repository-name/" : "",
-  basePath: isProd ? "/your-repository-name" : "",
-};
+
+const nextConfig = {};
 
 export default nextConfig;
